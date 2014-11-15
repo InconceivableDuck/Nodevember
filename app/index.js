df5209b9e818f000b09f7ada2c6274e6fd871d50
Brandon Cannaday <brandon@modulus.io>
2014-11-15 17:51:37 -0600

    Added app file.

---
+++ app/index.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('My port is ' + process.env.PORT);
+}).listen(process.env.PORT);
+console.log('Server running on port ' + process.env.PORT);
