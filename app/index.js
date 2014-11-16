2d42369159952ce0e141f53b0181a50d8a32b037
Brandon Cannaday <brandon@modulus.io>
2014-11-15 22:04:24 -0600

    Tweaked app.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('My port is ' + process.env.PORT);
+  res.end('My port is ' + process.env.PORT + '\n');
 }).listen(process.env.PORT);
 console.log('Server running on port ' + process.env.PORT);
